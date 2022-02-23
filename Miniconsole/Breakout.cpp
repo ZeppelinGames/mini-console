5b098437afd8a048e6cde2ee22fbf3f28e3967fe
Mark Saba <zeppelingames3@gmail.com>
2022-02-23 17:05:04 +1100

    started breakout prototype

--- Miniconsole/Breakout.cpp
+++ Miniconsole/Breakout.cpp
@@ -1,35 +1,82 @@
 #include "ConsoleGame.cpp"
 
-class Breakout : public ConsoleGame {
-    using ConsoleGame::ConsoleGame;
+class Breakout : public ConsoleGame
+{
+  using ConsoleGame::ConsoleGame;
 
-  public:
-    int x = 1;
-    int y = 1;
+public:
+  std::string GameName()
+  {
+    return "Breakout";
+  }
+  std::string GameDesc()
+  {
+    return "Break blocks using a paddle";
+  }
 
-    std::string GameName() {
-      return "Breakout";
-    }
-    std::string GameDesc() {
-      return "Break blocks using a paddle";
-    }
+  void Setup()
+  {
+    disp->disp.fillScreen(BLACK);
+    Serial.println("Setting up Breakout");
 
-    void Setup() {
-      disp->disp.fillScreen(BLACK);
-      Serial.println("Setting up Breakout");
-    }
+    blockPosX = new int[blocksX];
+    blockPosY = new int[blocksX];
 
-    void Draw() {
-      disp->disp.drawLine(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, x, y, random(0, 2) == 0 ? RED : random(0, 2) == 1 ? MAGENTA : YELLOW);
-      if (inputs[3]) {
-        x += 4;
+    int i = 0;
+    for (int y = 0; y < blockHeight * 6; y += blockHeight)
+    {
+      for (int x = 0; x < SCREEN_WIDTH; x += blockWidth)
+      {
+        blockPosX[i] = x;
+        blockPosY[i] = y;
+        blockColor[i] = disp->RandomColour();
+        blockState[i] = true;
+        i++;
       }
-      y++;
-      if (x > SCREEN_WIDTH) {
-        x = random(1, y);
-      }
-      if (y > SCREEN_HEIGHT) {
-        y = random(1, x);
+    }
+  }
+
+  void Draw()
+  {
+    for (int i = 0; i < blocksCount; i++)
+    {
+      if (blockState[i])
+      {
+        disp->DrawRect(blockPosX[i], blockPosY[i], blockWidth, blockHeight, blockColor[i]);
       }
     }
+
+    // LeFt
+    if (!(inputs[2] && inputs[4]))
+    {
+      if (inputs[2])
+      {
+        paddleX--;
+        paddleX = paddleX < 0 ? 0 : paddleX;
+      }
+
+      // right
+      if (inputs[4])
+      {
+        paddleX++;
+        paddleX = paddleX > 128 ? 128 : paddleX;
+      }
+    }
+  }
+
+private:
+  int ballX = SCREEN_WIDTH / 2;
+  int ballY = 32;
+
+  int paddleX = SCREEN_WIDTH / 2;
+  int paddleY = 16;
+
+  int blockPosX[];
+  int blockPosY[];
+  bool blockState[];
+  int blockColor[];
+
+  int blockWidth = SCREEN_WIDTH / 8;
+  int blockHeight = SCREEN_HEIGHT / 16;
+  int blocksCount = blockWidth * 6; // 8 creates width, 6 is amount of rows
 };
--- Miniconsole/Display.cpp
+++ Miniconsole/Display.cpp
@@ -2,26 +2,64 @@
 #include <Adafruit_SSD1351.h>
 #include <SPI.h>
 
-#define SCREEN_WIDTH  128
+#define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
 
-//Screen setup
+// Screen setup
 #define SCLK_PIN 18
 #define MOSI_PIN 23
-#define DC_PIN   16
-#define CS_PIN   17
-#define RST_PIN  5
+#define DC_PIN 16
+#define CS_PIN 17
+#define RST_PIN 5
 
-#define BLACK           0x0000
-#define BLUE            0x001F
-#define RED             0xF800
-#define GREEN           0x07E0
-#define CYAN            0x07FF
-#define MAGENTA         0xF81F
-#define YELLOW          0xFFE0
-#define WHITE           0xFFFF
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
 
-class Display {
-  public:
-    Adafruit_SSD1351 disp = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
+class Display
+{
+public:
+  void DrawLine(int x1, int y1, int x2, int y2, int c)
+  {
+    disp.drawLine(x1, y1, x2, y2, c);
+  }
+
+  void DrawRect(int x, int y, int scaleX, int scaleY, int c)
+  {
+    disp.drawRect(x, y, scaleX, scaleY, c);
+  }
+
+  int RandomColour()
+  {
+    int rndIndex = int(rnd() * 6);
+    switch (rndIndex)
+    {
+    case 0:
+      return BLUE;
+      break;
+    case 1:
+      return RED;
+      break;
+    case 2:
+      return GREEN;
+      break;
+    case 3:
+      return CYAN;
+      break;
+    case 4:
+      return MAGENTA;
+      break;
+    case 5:
+      return YELLOW;
+      break;
+    }
+  }
+
+private:
+  Adafruit_SSD1351 disp = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
 };
